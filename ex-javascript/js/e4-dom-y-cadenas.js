769ccf5c56be9043ba0cef384e1ef15ee6f0aac5
Sara <sarabor09@gmail.com>
2024-05-21 11:27:39 +0200

    add changes proyect istqb

--- ex-javascript/js/e4-dom-y-cadenas.js
+++ ex-javascript/js/e4-dom-y-cadenas.js
@@ -1,94 +1,29 @@
-//JAVASCRIPT DOM. OBTENER ELEMENTOS DE LA PAGINA POR SU ID
-//Obtener el elemento html con id ejemplo
-let parrafoEjemplo = document.getElementById("ejemplo");
-//Modificar el texto del elemento html con id ejemplo
-parrafoEjemplo.innerText = "Hola Bootcamp QA";
+let introduccion = "Hola que tal, bienvenido a Qa";
+let presentacion1 = document.getElementById("presentacion");
+presentacion1.innerHTML = introduccion;
 
-//Obtener el elemento html con id ejemploTitulo
-let tituloEjemplo = document.getElementById("ejemploTitulo");
-let miNombre = "Reyes";
-let miEdad = 33;
-tituloEjemplo.innerText = "Hola mi nombre es " + miNombre + " mi edad es " + miEdad;
+let longitud = document.getElementById("longitud");
+longitud.innerHTML = introduccion.length;
 
-//Obtener el elemento html con id ejemplo2 y agregar un texto con etiquetas html
-let ejemplo2 = document.getElementById("ejemplo2");
-ejemplo2.innerHTML = "<b>Ejemplo 2 en negrita</b>";
+let posicionQa = document.getElementById("posicionQa");
+posicionQa.innerHTML = introduccion.indexOf("Qa");
 
-//Crear un nuevo parrafo dentro del div con id contenido
-let divContenido = document.getElementById("contenido");
-let nuevoTitulo = document.createElement("h3");
-nuevoTitulo.innerText = "Ejemplo Crear Elementos desde Javascript";
-divContenido.appendChild(nuevoTitulo);
+let primeraLetra = document.getElementById ("primeraLetra");
+primeraLetra.innerHTML = introduccion.charAt(0);
 
-let nuevoParrafo = document.createElement("p");
-nuevoParrafo.innerText = "Hola soy un nuevo parrafo creado";
-divContenido.appendChild(nuevoParrafo);
+let ultimaLetra = document.getElementById ("ultimaLetra");
+ultimaLetra.innerHTML = introduccion.charAt(introduccion.length-1);
 
+let fraseMayuscula = document.getElementById ("fraseMayuscula");
+fraseMayuscula.innerHTML = introduccion.toUpperCase();
 
-//Obtener el atributo del elemento a con id ejemplo 3 y agregarle una url
-let ejemplo3 = document.getElementById("ejemplo3");
-let paginaWeb = "https://bootcampqa.com";
-ejemplo3.href = paginaWeb;
+let miNombre = "Sara";
+let saludo = "Hola " + miNombre;
+presentacion1.innerHTML = saludo;
 
-//Obtener el elemento html con id ejemplo4 y cambiar su color a rojo
-let ejemplo4 = document.getElementById("ejemplo4");
-let newColor = "red";
-ejemplo4.style.color = newColor;
-
-//CADENAS EN JAVASCRIPT
-//Escapar caracteres especiales en una cadena se hace con \
-let cadenaEjemplo = "Hola mi nombre es \"Juan\" que tal estas";
-let ejemplo5 = document.getElementById("ejemplo5");
-ejemplo5.innerText = cadenaEjemplo;
-
-//Contar caracteres de una cadena
-let ejemploContar = "Bienvenidos a Bootcamp QA";
-let totalLetrasCadenaEjemplo = ejemploContar.length;
-
-let ejemplo6 = document.getElementById("ejemplo6");
-ejemplo6.innerText = "La cadena " + ejemploContar + " tiene un total de " + totalLetrasCadenaEjemplo + "caracteres";
-
-//Obtener la letra en la posicion que elijas de la cadena
-let primeraLetra = ejemploContar[0]; //la primera siempre empieza en 0
-let ultimaLetra = ejemploContar[ejemploContar.length - 1]; //El total de letras de la cadena, menos uno porque empieza en 0
-let ejemplo7 = document.getElementById("ejemplo7");
-ejemplo7.innerText = "La primera letra de la palabra " + ejemploContar + " es " + primeraLetra + " y la ultima letra es " + ultimaLetra;
-
-//Cambiar la cadena todo a mayusculas o minusculas
-let cadenaMayusculas = ejemploContar.toUpperCase();
-let cadenaMinusculas = ejemploContar.toLowerCase();
-
-let ejemplo8 = document.getElementById("ejemplo8");
-ejemplo8.innerText = "La cadena " + ejemploContar + " mayusculas " + cadenaMayusculas + " minusculas " + cadenaMinusculas;
-
-//sustituir texto de una cadena
-//Sustituir una palabra por otra
-let nuevaCadena = ejemploContar.replace("Bootcamp QA","Mi web");
-let ejemplo9 = document.getElementById("ejemplo9");
-ejemplo9.innerText = "Antes de reemplazar " + ejemploContar + " despues de reemplazar " + nuevaCadena;
-//Sustituir una palabra por una variable
-let mensajeBienvenida = "Bienvenido estudiante";
-let nombreEstudiante = "Juan";
-let nuevoMensajeBienvenida = mensajeBienvenida.replace("estudiante", nombreEstudiante);
-let ejemplo10 = document.getElementById("ejemplo10");
-ejemplo10.innerText = "Antes de reemplazar " + mensajeBienvenida + " despues de reemplazar " + nuevoMensajeBienvenida;
+let nuevaFrase = document.getElementById ("nuevaFrase");
+nuevaFrase.innerHTML = introduccion.substring(0 , introduccion.indexOf(" que tal")) + introduccion.substring(introduccion.indexOf(", bienvenido"), introduccion.length);
 
 
-//CADENAS
 
 
-let presentacion = "Hola que tal,bienvenida a QA";
-let tamanyo = presentacion.length;
-let ejercicio1 = document.getElementById("ejercicio1");
-ejercicio1.innerHTML = presentacion + "<br> tamaño=" + tamanyo; 
-
-let positionQA = presentacion.indexOf("QA");
-let ejercicio2 = document.getElementById("ejercicio2");
-ejercicio2.innerHTML = "position=" + positionQA; 
-
-
-let ejercicio3 = document.getElementById("ejercicio3");
-ejercicio3.innerHTML = "primera letra =" + presentacion[0]; 
-
-let ejercicio4 = document.getElementById("ejercicio4");
-ejercicio4.innerHTML = "ultima letra=" + presentacion[tamanyo-1];
--- ex-javascript/script.js
+++ ex-javascript/script.js
@@ -1,1 +1,23 @@
-document.write('HOLA MUNDO');
+function welcome(){ 
+    let name = document.getElementById("name").value;
+    
+    if (name.length > 0){
+        let message = "Welcome " + name;
+        alert (message);
+    }
+}
+
+function send(){
+    let correctAnswer = "option1";
+    let answer = document.getElementsByName("option").value;
+    let messageCorrect = "Correct answer";
+    let messageWrong = "Wrong answer";
+    
+    if (correctAnswer == answer){
+        alert (messageCorrect);
+    } else {
+        alert (messageWrong);
+    }
+
+
+}
