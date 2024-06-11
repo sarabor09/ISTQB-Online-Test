73bf8f2a96298f60ade58daa335b17cf99242263
Sara <sarabor09@gmail.com>
2024-06-11 19:48:53 +0200

    add arrays

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
-let correctAnswer1 = "option1";
-let correctAnswer2 = "option3";
-let correctAnswer3 = "option3";
-let correctAnswer4 = "option2";
-let correctAnswer5 = "option1";
-let correctAnswer6 = "option3";
-let correctAnswer7 = "option3";
-let correctAnswer8 = "option2";
-let correctAnswer9 = "option3";
-let correctAnswer10 = "option2";
-
-
+let correctAnswerArray = ["option1", "option3","option3", "option2", "option1", "option3", "option3", "option2","option3", "option2"];
 
 
 function welcome(){ 
@@ -26,86 +15,31 @@ function send(numQuestion){
     let option = document.querySelector('input[name="option'+numQuestion+'"]:checked');
     let messageCorrect = "Correct answer 2 points";
     let messageWrong = "Wrong answer";
+    let answerText = document.getElementById("answerText"+numQuestion);
 
-    switch(numQuestion){
-        case 1: 
-            correctAnswer = correctAnswer1;
-            break;
-        case 2:
-            correctAnswer = correctAnswer2;
-            break;
-        case 3:
-            correctAnswer = correctAnswer3;
-            break;
-        case 4: 
-            correctAnswer = correctAnswer4;
-            break;
-        case 5:
-            correctAnswer = correctAnswer5;
-            break;
-        case 6:
-            correctAnswer = correctAnswer6;
-            break;
-        case 7: 
-            correctAnswer = correctAnswer7;
-            break;
-        case 8:
-            correctAnswer = correctAnswer8;
-            break;
-        case 9:
-            correctAnswer = correctAnswer9;
-            break;
-        case 10:
-            correctAnswer = correctAnswer10;
-            break;
-    }
-    
+    correctAnswer = correctAnswerArray[numQuestion - 1];
+
     if(option == null){
-        alert(messageWrong + " 0 points");
+        answerText.innerText = messageWrong + " 0 points";
     } 
     else if (correctAnswer == option.id){
-        alert (messageCorrect);
+        answerText.innerText = messageCorrect;
     } else {
-        alert (messageWrong + " -1 points");
+        answerText.innerText = messageWrong + " -1 points";
     }
 
 }
-function showAnswer(numQuestion){
-    let correctAnswer;
 
-    switch(numQuestion){
-        case 1: 
-            correctAnswer = document.querySelector('input[name="option'+numQuestion+'"][id="'+correctAnswer1+'"]');
-            break;
-        case 2:
-            correctAnswer = document.querySelector('input[name="option'+numQuestion+'"][id="'+correctAnswer2+'"]');
-            break;
-        case 3:
-            correctAnswer = document.querySelector('input[name="option'+numQuestion+'"][id="'+correctAnswer3+'"]');
-            break;
-        case 4:
-            correctAnswer = document.querySelector('input[name="option'+numQuestion+'"][id="'+correctAnswer4+'"]');
-            break;
-        case 5:
-            correctAnswer = document.querySelector('input[name="option'+numQuestion+'"][id="'+correctAnswer5+'"]');
-            break;
-        case 6:
-            correctAnswer = document.querySelector('input[name="option'+numQuestion+'"][id="'+correctAnswer6+'"]');
-            break;
-        case 7:
-            correctAnswer = document.querySelector('input[name="option'+numQuestion+'"][id="'+correctAnswer7+'"]');
-            break;
-        case 8:
-            correctAnswer = document.querySelector('input[name="option'+numQuestion+'"][id="'+correctAnswer8+'"]');
-            break;
-        case 9:
-            correctAnswer = document.querySelector('input[name="option'+numQuestion+'"][id="'+correctAnswer9+'"]');
-            break;
-        case 10:
-            correctAnswer = document.querySelector('input[name="option'+numQuestion+'"][id="'+correctAnswer10+'"]');
-            break;
+function showAnswerText(numQuestion, solution){
+    let answerText = document.getElementById("answerText"+numQuestion);
+    answerText.innerText = "Correct answer is " + solution;
+}
+
+function reset(numQuestion){
+    let option = document.querySelector('input[name="option'+numQuestion+'"]:checked');
+    if(option != null){
+        option.checked = false;
     }
-    
-    correctAnswer.checked = true;  
-
+    let answerText = document.getElementById("answerText"+numQuestion);
+    answerText.innerText = "";
 }
