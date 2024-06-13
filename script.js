e4f19c1926d49a02a95b4ff473e91718e169d63f
Sara <sarabor09@gmail.com>
2024-06-13 20:38:01 +0200

    add ranking and finish test

--- script.js
+++ script.js
@@ -1,12 +1,37 @@
-let correctAnswerArray = ["option1", "option3","option3", "option2", "option1", "option3", "option3", "option2","option3", "option2"];
-
+const correctAnswerArray = ["option1", "option6","option9", "option11", "option13", 
+"option18", "option21", "option23","option27", "option29"];
+let rankingList = [
+    {
+        name : "Sara",
+        result : 5
+    },
+    {
+        name : "Kike",
+        result : 3
+    },
+    {
+        name : "Yago",
+        result : 7
+    },
+    {
+        name : "Leo",
+        result : 9
+    }
+];
 
 function welcome(){ 
-    let name = document.getElementById("name").value;
+    let userName = document.getElementById("name").value;
     let welcomeName = document.getElementById("welcomeName")
-    if (name.length > 0){
-        let message = "Welcome to ISTQB Online Test " + name;
+    let finishButton = document.getElementById("finishButton");
+
+    if (userName.length > 0){
+        let message = "Welcome to ISTQB Online Test " + userName;
         welcomeName.innerText = message;
+        finishButton.disabled = false;
+
+    } else {
+        finishButton.disabled = true;
+        welcomeName.innerText = "";
     }
 }
 
@@ -16,6 +41,7 @@ function send(numQuestion){
     let messageCorrect = "Correct answer 2 points";
     let messageWrong = "Wrong answer";
     let answerText = document.getElementById("answerText"+numQuestion);
+    let points = 0;
 
     correctAnswer = correctAnswerArray[numQuestion - 1];
 
@@ -24,10 +50,13 @@ function send(numQuestion){
     } 
     else if (correctAnswer == option.id){
         answerText.innerText = messageCorrect;
+        points = 2;
     } else {
         answerText.innerText = messageWrong + " -1 points";
+        points = -1;
     }
 
+    return points;
 }
 
 function showAnswerText(numQuestion, solution){
@@ -42,4 +71,57 @@ function reset(numQuestion){
     }
     let answerText = document.getElementById("answerText"+numQuestion);
     answerText.innerText = "";
+}
+
+function finish() {
+
+    let userResult = 0;
+    let resultText = document.getElementById("result");
+    let userName = document.getElementById("name").value;
+
+    for (let numQuestion = 1; numQuestion <= 10; numQuestion++) {
+
+        userResult = userResult + send(numQuestion);
+    }
+
+    resultText.innerText = "You're result is " + userResult + " points."
+
+    let newUser = {
+        name : userName,
+        result : userResult
+    }
+
+    rankingList.push(newUser);
+
+}
+
+function resetAll() {
+
+    let resultText = document.getElementById("result");
+
+    for (let numQuestion = 1; numQuestion <= 10; numQuestion++) {
+
+        reset(numQuestion);
+    }
+
+    resultText.innerText = "";
+}
+
+function showRanking() {
+    let rankingListContainer = document.getElementById("rankingList");
+    rankingListContainer.innerHTML = "";
+
+    rankingList.sort((a, b) => b.result - a.result)
+
+    let row ="<ol>"
+    rankingList.forEach( user => {
+         row = row + "<li>" + user.name + " --> " + user.result + "</li>";
+        
+    })
+
+    row = row + "<ol>"
+    rankingListContainer.innerHTML = row;
+
+    let rankingContainer = document.getElementById("ranking");
+    rankingContainer.hidden = false;
 }
